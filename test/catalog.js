49750782651d25a4b3c3085f43988c5b1a4b028e
Bob Corsaro <bob@embed.ly>
2012-04-01 15:25:52 -0400

    Adds test for catalog

---
+++ test/catalog.js
@@ -0,0 +1,38 @@
+var catalog = require('../lib/catalog.js')
+  , should = require('should')
+  , step = require('step')
+  , libxmljs = require('libxmljs')
+  , Hash = require('hashish');
+
+describe('render', function() {
+
+  function setup() {
+    var mock = {
+        res: {},
+        req: { endpointParams: {callback: 'c'} },
+        config: {
+          endpoints: [{config: {name: 'a'}}, {config: {name: 'b'}}]
+        }
+    }
+    mock.handler = catalog(mock.config)
+    return mock
+  }
+
+  it('should render catalog of endpoints', function(done) {
+    var mock = setup();
+
+    mock.res.render = function(view, payload) {
+      view.should.match(/doc.jade/);
+      payload.errors.length.should.eql(0);
+      payload.endpoints.length.should.eql(2);
+      payload.endpoints[0].should.eql({name: 'a'});
+      payload.endpoints[1].should.eql({name: 'b'});
+      payload.should.have.property('Hash', Hash);
+      payload.config.should.have.property('stylesheets');
+      payload.config.should.have.property('view');
+      payload.config.should.have.property('endpoints', mock.config.endpoints);
+      done();
+    }
+    mock.handler(mock.req, mock.res)
+  });
+});
